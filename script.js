c8fab4c872de5bc31f7dd3c35bbbb3ece6dec9ac
TrucPhuong-1906194 <1906194@lsts.edu.vn>
2025-03-19 09:04:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var countDownDate = new Date("Nov 27, 2026 22:57:00").getTime();
-var x = setInterval(function(){
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+//xác định
+var countDownDate = new Date("Nov 27, 2026 22:57:00").getTime(); //Chuyển ngày 27/11/2026 22:57:00 thành đối tượng ngày giờ...
+var x = setInterval(function(){                                  //Lấy timestamp
+    var now = new Date().getTime();                              //Gọi hàm mỗi giây (1000ms) để cập nhật thời gian còn lại.
+    var distance = countDownDate - now;                          //Tính thời gian còn lại => công thức tính biến distance
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -20,28 +21,29 @@ var x = setInterval(function(){
         document.getElementById("minutes").innerHTML = "00";
         document.getElementById("seconds").innerHTML = "00";
     }
-},1000)
+},1000)                                                         //function cập nhật mỗi giây
 
-const clickButton = document.querySelector(".button")
+const clickButton = document.querySelector(".button")           //không đổi
 clickButton.addEventListener("click", function(){
     window.open("https://product-card-web.vercel.app/", "_blank") //Mở một liên kết (URL) mới "_blank" cho phép trang mở ở tab mới;
+                                                                  //window.open(url, target, options)
 })
 
-window.onload = function() {
-    const text = "Welcome to My Website!";
-    let index = 0;
-    function typeEffect() {
+window.onload = function() {                                    //chạy hiệu ứng khi trang web mở lên
+    const text = "Welcome to My Website!";                      //string, dùng const khai
+    let index = 0;                                              // vị trí
+    function typeEffect() { //On repeat
         if (index < text.length) {
-            document.getElementById("typewriter").innerHTML += text.charAt(index);
+            document.getElementById("typewriter").innerHTML += text.charAt(index); //Hiện từng chữ; cập nhật vào HTML
             index++;
-            setTimeout(typeEffect, 100);
+            setTimeout(typeEffect, 100);                         //dừng 0.1s mỗi chữ tạo hiệu ứng
         } else {
             setTimeout(() => {
-                document.getElementById("typewriter").innerHTML = "";
+                document.getElementById("typewriter").innerHTML = ""; //xóa và lặp lại
                 index = 0;
-                typeEffect();
+                typeEffect();                                         //chạy function
             }, 2000);
         }
     }
-    typeEffect(); // Start the effect
+    typeEffect(); // Start the effect khi trang web mở
 };
